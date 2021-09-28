9f4a4f47b8f7c21559abb19ed07afddae0f609ba
Tariq Abdulghnai <tariq.abdulghani.hussein@gmail.com>
2021-09-28 20:55:50 +0200

    added save and delete end points

--- java/com/example/demo/todo/TodoController.java
+++ java/com/example/demo/todo/TodoController.java
@@ -2,16 +2,17 @@ package com.example.demo.todo;
 
 import java.util.Set;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoController {
 	
 	private final TodoService todoService;
-	
 	
 	
 	public TodoController(TodoService todoService) {
@@ -27,5 +28,15 @@ public class TodoController {
 	@GetMapping("/todo/{id}")
 	Todo getById(@PathVariable Long id){
 		return this.todoService.getById(id);
+	}
+	
+	@PostMapping("/todo")
+	Todo add(@RequestBody Todo todo) {
+		return todoService.save(todo);
+	}
+	
+	@DeleteMapping("/todo/{id}")
+	void remove(@PathVariable Long id) {
+		this.todoService.removeById(id);
 	}
 }
