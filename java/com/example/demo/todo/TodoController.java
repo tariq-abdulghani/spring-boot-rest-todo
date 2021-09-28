1887e37bec1591cf2332a4e855404a2d444f020a
Tariq Abdulghnai <tariq.abdulghani.hussein@gmail.com>
2021-09-28 20:35:49 +0200

    initialization of project

--- java/com/example/demo/todo/TodoController.java
+++ java/com/example/demo/todo/TodoController.java
@@ -3,6 +3,8 @@ package com.example.demo.todo;
 import java.util.Set;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,10 +19,13 @@ public class TodoController {
 		this.todoService = todoService;
 	}
 
-
-
 	@GetMapping("/todo")
 	Set<Todo> getAll(){
 		return this.todoService.getAll();
+	}
+	
+	@GetMapping("/todo/{id}")
+	Todo getById(@PathVariable Long id){
+		return this.todoService.getById(id);
 	}
 }
