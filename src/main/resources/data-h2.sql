bf126dff4fd927b1ad66f1be1132182228fc37c8
Tariq Abdulghnai <tariq.abdulghani.hussein@gmail.com>
2021-10-02 21:24:26 +0200

    modified properties file to allow hibernate to validate schema, modified sql file to add new columns

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,14 +1,16 @@
 drop table if exists todos;
 
 create table todos(
-  id INT AUTO_INCREMENT  PRIMARY KEY,
+  id BIGINT AUTO_INCREMENT  PRIMARY KEY,
   title VARCHAR(250) NOT NULL,
   description VARCHAR(250) NOT NULL,
-  creation_date Date 
+  creation_date Date,
+  last_modified Date,
+  status VARCHAR(100)
 );
 
-insert into todos(id, title, description, creation_date) values(1, 'todo1', 'todo1 desc', '2021-09-27');
-insert into todos(id, title, description, creation_date) values(2, 'todo2', 'todo2 desc', '2021-09-27');
-insert into todos(id, title, description, creation_date) values(3, 'todo3', 'todo3 desc', '2021-09-27');
-insert into todos(id, title, description, creation_date) values(4, 'todo4', 'todo4 desc', '2021-09-27');
-insert into todos(id, title, description, creation_date) values(5, 'todo5', 'todo5 desc', '2021-09-27');
+insert into todos(id, title, description, creation_date, last_modified, status) values(1, 'todo1', 'todo1 desc', '2021-09-27', '2021-09-27', 'TODO');
+insert into todos(id, title, description, creation_date, last_modified, status) values(2, 'todo2', 'todo2 desc', '2021-09-27', '2021-09-27', 'TODO');
+--insert into todos(id, title, description, creation_date) values(3, 'todo3', 'todo3 desc', '2021-09-27');
+--insert into todos(id, title, description, creation_date) values(4, 'todo4', 'todo4 desc', '2021-09-27');
+--insert into todos(id, title, description, creation_date) values(5, 'todo5', 'todo5 desc', '2021-09-27');
